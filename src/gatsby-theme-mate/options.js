44ebe8a32b856cead25c6643bb0df25a8fd87e4f
thabangmoyo <92871698+thabangmoyo@users.noreply.github.com>
2021-11-03 06:05:57 +0200

    Update and rename .empty to options.js

---
+++ src/gatsby-theme-mate/options.js
@@ -0,0 +1,34 @@
+export default {
+  topbar: {
+    sticky: true,
+    maxWidth: 1260,
+  },
+  header: {
+    sticky: true,
+    stickyMobile: true,
+    maxWidth: 1260,
+    mobileNavWidth: 300,
+    mobileAnimation: "fade", // fade, fadeInUp, fadeInDown, slideRight, slideLeft
+    spring: { tension: 170, friction: 26 }, // React Spring config object for your MobileNav
+  },
+  sideNav: {
+    width: "18em",
+    spring: { tension: 170, friction: 26 }, // spring config for your responsive SideNav
+  },
+  content: {
+    maxWidth: 1020,
+    gridGap: 30,
+  },
+  sidebar: {
+    width: ".3fr",
+  },
+  footer: {
+    maxWidth: 1020,
+    gridGap: 30,
+  },
+  breakpoints: {
+    sm: 750,
+    md: 960,
+    lg: 1240,
+  },
+}
